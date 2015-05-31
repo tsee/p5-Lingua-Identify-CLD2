74c3762235b349355ef8432d71c397780d3124b7
Steffen Mueller <smueller@cpan.org>
2015-05-31 12:38:52 +0200

    s/Example/CLD2/g

--- lib/Lingua/Identify/CLD2.pm
+++ lib/Lingua/Identify/CLD2.pm
@@ -1,4 +1,4 @@
-package XSpp::Example;
+package Lingua::Identify::CLD2;
 use 5.008;
 use strict;
 use warnings;
@@ -6,30 +6,18 @@ use warnings;
 our $VERSION = '0.01';
 
 require XSLoader;
-XSLoader::load('XSpp::Example', $VERSION);
+XSLoader::load('Lingua::Identify::CLD2', $VERSION);
 
 1;
 __END__
 
 =head1 NAME
 
-XSpp::Example - A simple example of XS++
+Lingua::Identify::CLD2 - CLD2 wrapper for Perl
 
 =head1 DESCRIPTION
 
-This module just serves as a very basic example distribution using C<ExtUtils::XSpp>
-to wrap C++ code for use from Perl. See F<Animal.h> and F<Dog.h> for the
-C++ implementation and F<Animal.xsp> and F<Dog.xsp> for the declaration of the
-interface.
-
-Types are mapped in F<typemap.xsp> (XS++ type map)
-and F<mytype.map> as well as F<perlobject.map> (XS type map).
-
-The classes are used in the test files under F<t/>.
-
 =head1 SEE ALSO
-
-L<ExtUtils::XSpp>
 
 =head1 AUTHOR
 
@@ -37,9 +25,9 @@ Steffen Mueller, E<lt>smueller@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-The XSpp::Example module is
+The C<Lingua::Identify::CLD2> module is
 
-Copyright (C) 2010 by Steffen Mueller
+Copyright (C) 2015 by Steffen Mueller
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.0 or,
